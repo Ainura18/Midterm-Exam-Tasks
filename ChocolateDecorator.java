365abaf0f3e70dfcdba47096468a60abba6cfe20
Ainura18 <157133099+Ainura18@users.noreply.github.com>
2025-03-16 05:25:07 +0500

    Create ChocolateDecorator.java

---
+++ ChocolateDecorator.java
@@ -0,0 +1,15 @@
+public class ChocolateDecorator extends CoffeeDecorator {
+    public ChocolateDecorator(Coffee coffee) {
+        super(coffee);
+    }
+
+    @Override
+    public String getDescription() {
+        return decoratedCoffee.getDescription() + ", Chocolate";
+    }
+
+    @Override
+    public double getCost() {
+        return decoratedCoffee.getCost() + 0.7;
+    }
+}
