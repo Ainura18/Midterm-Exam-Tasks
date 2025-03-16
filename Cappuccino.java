699c80cf7a01ee075dc642264b4668902e8d326f
Ainura18 <157133099+Ainura18@users.noreply.github.com>
2025-03-16 05:21:53 +0500

    Create Cappuccino.java

---
+++ Cappuccino.java
@@ -0,0 +1,12 @@
+public class Cappuccino implements Coffee {
+    @Override
+    public String getDescription() {
+        return "Cappuccino";
+    }
+
+    @Override
+    public double getCost() {
+        return 4.0;
+    }
+}
+
